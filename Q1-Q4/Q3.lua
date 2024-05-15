78fceffa339ad7c71fcdad6d68b395ef6e0b658f
jacoposim <127415884+jacoposim@users.noreply.github.com>
2024-05-15 10:08:34 +0200

    Update comments Q3.lua
    I noticed that Lua multi-line comments are not displayed well on GitHub, so I changed them to single-line comments.

--- Q1-Q4/Q3.lua
+++ Q1-Q4/Q3.lua
@@ -11,13 +11,13 @@ function do_sth_with_PlayerParty(playerId, membername)
 end
 
 
---[[
-	Edits:
-		- making the variable player local
-		- checks on variables player and party to avoid possible null pointer errors
-		- removing of the loop to remove a player from the party: having already the membername as a function input
-		  parameter, we don't need to loop into all party members
---]]
+
+-- Edits:
+-- 	- making the variable player local
+-- 	- checks on variables player and party to avoid possible null pointer errors
+-- 	- removing of the loop to remove a player from the party: having already the membername as a function input
+-- 	  parameter, we don't need to loop into all party members
+
 
 function do_sth_with_PlayerParty(playerId, membername)
     local player = Player(playerId)
